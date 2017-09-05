8cd99ca8012f190c56bb335ac7fdeab08c65d924
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2017-09-05 14:55:03 -0300

    Logout route added

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -52,4 +52,16 @@ router.post('/login', (req, res, next) => {
     }
 })
 
+router.get('/logout', (req, res, next) => {
+    if (req.session) {
+        /*
+        Delete session object
+        */
+        req.session.destroy((err) => {
+            if (err) { return next(err)}
+            else { return res.redirect('/')}
+        })
+    }
+})
+
 module.exports = router
